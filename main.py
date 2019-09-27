58d6f3bf2d9799a513423e27def00af65ec29882
Nastassia Davydzenka <375445014898@mail.ru>
2019-09-27 13:41:29 +0300

    add other classes

---
+++ main.py
@@ -0,0 +1,61 @@
+from Dev import Dev
+from Project import Project
+from Task import Task
+
+
+def main():
+    """Создание объектов"""
+    project_mazad = Project('Mazad')
+    project_bosh = Project('Bosh')
+    task1 = Task('create_database', 5, "t")
+    print(task1.priority_task)
+    task2 = Task('second_task', 3)
+    task3 = Task('second_task', 3)
+    task4 = [Task('fourth', 3),
+             Task('five_task', 3)]
+    task5 = [Task('seven', 3),
+             Task('eight', 3)]
+    dev1 = Dev('developerMitya@mail.ru', '111111', 'Oleg', 'Fomin', 22)
+    dev2 = Dev('Nastya@mail.ru', '111111', 'Oleg', 'Fomin', 22)
+    print(task1)
+    print(task1.priority_task)
+    task1.change_status_task('Completed')
+    print(task1)
+
+
+
+    """использование методов
+    task1.change_status_task('Close')
+    task1.change_priority_task(2)
+    dev1.add_task(task2)
+    dev1.add_task(task3)
+    dev1.add_task(task5)
+    print(dev1)# если мы прописываем переменную задачи при создании, то выводится обработанная информация,
+    # если добавляем - то ссылка на объект
+    #print(task1)
+    print(dev1.show_all_dev_task())
+    task2.add_executor_for_task(dev1)
+    print(task2)
+    dev1.add_task(task4)
+    project_bosh.add_task_to_project(task4)
+    print(project_bosh.task)
+    project_bosh.add_task_to_project(task1)
+    print(dev1.task)
+    print(project_bosh.task) выводятся ссылки на объекты, а хочу чтобы обрабатывались
+    
+    <test> add tast to project 
+    
+    project_bosh.add_task_to_project(task4)
+    print(project_bosh.task)
+    project_bosh.add_task_to_project(task1)
+    print(project_bosh)
+    
+    print(Task)"""
+
+
+if __name__ == '__main__':
+    main()
+
+#  print(str(task1))
+# print("\n", task1, "\n", task2, "\n", task3, "\n", dev1)
+#   dev1.add_task(task1)
